70f35db7814cdaa78d63ea7672cc30712e30501f
Whitney Akred <whitneyakred@Whitneys-MacBook-Air.local>
2026-03-04 14:38:14 -0600

    array-change

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
-    Hello Vue
-  </div>
+  <h1>{{ message }}</h1>
+  <button @click="sortUsersByAge">Sort users by age</button>
+  <button @click="hideInactiveUsers">Hide inactive users</button>
+  <button @click="showFirstTwoUsers">Show first two users</button>
+  <ul>
+    <li v-for="(user, index) in users" :key="user.id">
+      {{ index }} - {{ user.id }} - {{ user.name }} - {{ user.age }} -
+      {{ user.isActive }}
+    </li>
+  </ul>
 </template>
 
-<script setup>
 
+<script setup>
+import { ref } from 'vue'
+
+let message =ref('Hello, Array Change Detection!')
+
+const users =ref([
+  { id: 1001, name: 'John Smith', age: 26, isActive: false },
+  { id: 1002, name: 'Tom Doe', age: 16, isActive: false },
+  { id: 1003, name: 'Frankin Wong', age: 18, isActive: true }
+])
+
+function sortUsersByAge() {
+  users.value.sort((a, b) => a.age - b.age)
+}
+function hideInactiveUsers() {
+  users.value =users.value.filter((user) => user.isActive)
+}
+function showFirstTwoUsers() {
+  users.value =users.value.slice(0, 2)
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
 </style>
