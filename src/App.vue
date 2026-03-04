fd17dc6ae06444d4d1e796f8f8355dc2c05fb5c5
Whitney Akred <whitneyakred@Whitneys-MacBook-Air.local>
2026-03-04 14:46:06 -0600

    v-if

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    Hello Vue
-  </div>
+
+  <h1>{{ message }}</h1>
+
+  <p>
+    What has keys but can't open locks, space but no room, and you can enter but
+    can't go outside? What am I?
+  </p>
+
+  <button style="width: 200px" @click="showAnswer = !showAnswer">
+    {{ !showAnswer ? 'Show me the answer!' : 'Hide the answer!' }}
+  </button>
+
+  <div v-if ="showAnswer">A keyboard</div>
+  <!-- <div v-if="Math.random() > 0.5">Now you see me</div>
+  <div v-else>Now you don't</div>-->
+  <div v-show="showAnswer">A keyboard.</div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+let message =ref('Hello, v-if!')
+let showAnswer =ref(false)
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
