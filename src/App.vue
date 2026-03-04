bfdc2af70e57314e0504b26ec83d833d879a98d3
Whitney Akred <whitneyakred@Whitneys-MacBook-Air.local>
2026-03-04 14:44:02 -0600

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,57 @@
 <template>
   <div>
-    Hello Vue
+    <h1>{{ message }}</h1>
+    <img v-bind:src="imageUrl" alt="" />
+    <br/>
+
+    <img :src="imageUrl" alt="" />
+    <button @click="changeImage">Change Image</button>
+    <hr/>
+
+    <input type="text" :value="defaultInputText"/>
+
+    <hr/>
+    <p :class="className">Harry Potter</p>
+    <p :class="{inactive: isInactive, center: isCenter}">
+      Harry Potter
+    </p>
+    <p :class="['active','center']">Harry Potter</p>
   </div>
+
+
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+let message='Hello,v-bind!'
+let imageUrl=ref('/public/img/banner_1.jpg')
+function changeImage(){
+  imageUrl.value='/public/img/banner_2.jpg'
+}
+
+let defaultInputText='Write something here...'
+let className='active'
+let isInactive=ref(true)
+let isCenter=ref(false)
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+img {
+  max-width: 300px;
+}
 
+.active {
+  color: green;
+}
+
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+
+.center {
+  text-align: center;
+}
 </style>
