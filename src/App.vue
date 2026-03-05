bd9162a2e8fafebf4f473053e108c9f124ae5bdb
Whitney Akred <whitneyakred@Whitneys-MacBook-Air.local>
2026-03-05 12:32:09 -0600

    bind-v-model

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
+  <h1>v-model vs v-bind</h1>
+  <h2>message: {{ message }}</h2>
+
   <div>
-    Hello Vue
+    <p>v-model =v-bind +@input</p>
+    <input
+      type="text"
+      v-bind:value="message"
+      @input="message = $event.target.value"
+    />
+  </div>
+
+  <div>
+    <p>v-model</p>
+    <input type="text" v-model="message" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+let message =ref('Hello!')
 </script>
 
-<style lang="scss" scoped>
 
-</style>
+<style scoped></style>
