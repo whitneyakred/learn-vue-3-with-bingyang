7f8c5b4e127003bc4c87ed0d53e3bec79e500f18
Whitney Akred <whitneyakred@Whitneys-MacBook-Air.local>
2026-03-09 09:46:07 -0500

    lifestyle

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,78 @@
 <template>
-  <div>
-    Hello Vue
+  <h1>{{ message }}</h1>
+  <div class="card">
+
+    <h2 ref="title">This is the App component.</h2>
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">Increment number by one</button>
+    <button @click="isShow =!isShow">Toggle component1</button>
+    <Component1 v-if="isShow"></Component1>
+
   </div>
 </template>
 
 <script setup>
+import {
 
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  watch
+
+} from 'vue'
+
+import Component1 from './Component1.vue'
+
+let message =ref('Hello, Lifecycle Hooks!')
+
+let number =ref(1)
+let title =ref(null)
+
+let isShow =ref(true)
+
+console.log('App component is setup')
+
+onBeforeMount(() => {
+  console.log('App component is before mount')
+  console.log(number.value) 
+  console.log(title.value)
+})
+
+onMounted(() => {
+  console.log('App component is mounted')
+  console.log(title.value) 
+})
+
+onBeforeUpdate(() => {
+  console.log('App component is before update')
+})
+
+onUpdated(() => {
+  console.log('App component is updated')
+})
+
+onBeforeUnmount(() => {
+  console.log('App component is before unmount')
+})
+
+onUnmounted(() => {
+  console.log('App component is unmounted')
+})
+
+watch(number, () => {
+  console.log('number changes!')
+})
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.card {
+  background-color: purple;
+  color: white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
 </style>
