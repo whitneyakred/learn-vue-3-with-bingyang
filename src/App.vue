d035d7225c1ec9d7804932002403c708333068f1
Whitney Akred <whitneyakred@Whitneys-MacBook-Air.local>
2026-03-12 16:23:38 -0500

    props

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,68 @@
 <template>
-  <div>
-    Hello Vue
-  </div>
+  <BlogPost id="1" 
+  blogPostTitle="What is a muggle?"
+  blogPostContent="A muggle is a person without any magic"></BlogPost>
+
+  <BlogPost id="1" 
+  blog-post-title="What is a muggle?"
+  blog-post-content="A muggle is a person without any magic"></BlogPost>
+
+  <hr/>
+   <BlogPost :id="blogPostId"
+  :blog-post-title="blogPostTitle"
+  :blog-post-content="blogPostContent"></BlogPost>
+  <hr/>
+
+  <Blog-Post v-for="post in posts" 
+  :key ="post.id"
+  :id ="post.id" 
+  :blog-post-title ="post.blogPostTitle"
+  :blog-post-content ="post.blogPostContent"></Blog-Post>
+
+  <hr/>
+  <Blog-Post v-for ="post in posts" 
+  :key ="post.id"
+  v-bind ="post"></Blog-Post>
 </template>
 
 <script setup>
+import {ref} from 'vue'
+import BlogPost from './BlogPost.vue';
 
+let blogPostId =ref(2)
+let blogPostTitle =ref("Harry Potter...")
+let blogPostContent = ref("words words words")
+
+let posts =ref([
+  {
+    id: 1,
+    blogPostTitle: 'What is a muggle in HP world?',
+    blogPostContent:
+      'A muggle is a person who lacks any sort of magical ability...'
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      'HP and the Cursed Child Broadway production suspended until April',
+    blogPostContent:
+      'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Potter DIY: Make Your Own “Harry Potter Puppet Pal”',
+    blogPostContent:
+      'We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Fan Project Brings Life to “Harry Potter” in Translation',
+    blogPostContent:
+      'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
+  }
+  
+])
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
---
+++ src/BlogPost.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="blog-post">
+        <p>{{ message }}</p>
+    </div>
+    <h2>{{ id }}- {{  blogPostTitle }}</h2>
+    <h4>{{  blogPostContent }}</h4>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+let message =ref('This is the Blog Post')
+const props =defineProps(['id','blogPostTitle','blogPostContent'])
+console.log(props.blogPostTitle)
+
+</script>
+
+<style scoped>
+
+.blog-post{
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+</style>
