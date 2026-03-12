bfabe2aa718db60377f26a8b567849cc32651598
Whitney Akred <whitneyakred@Whitneys-MacBook-Air.local>
2026-03-12 16:36:34 -0500

    v-model

---
+++ src/BlogPost.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="blog-post">
+    <p>
+      {{ message }}
+    </p>
+
+    <h2>{{ id }} - {{ blogPostTitle }}</h2>
+    <h4>{{ blogPostContent }}</h4>
+
+    <div>
+      Modify the title
+       <input type="text" v-model="blogPostTitle" />
+    </div>
+
+    <h4>{{ blogPostContent }}</h4>
+    
+    <div>
+      Modify the content:
+      <input type="text" v-model="blogPostContent" />
+    </div>
+
+    <button @click="$emit('delete-blog-post', id)">Delete post</button>
+    <button @click="emitDeletePostEvent(id)">Delete post</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message =ref('This is the BlogPost component.')
+defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+
+const emit =defineEmits(['delete-blog-post'])
+
+function emitDeletePostEvent(id) {
+  emit('delete-blog-post', id)
+}
+
+let blogPostTitle =defineModel('blogPostTitle') 
+let blogPostContent =defineModel('blogPostContent') 
+
+</script>
+
+<style scoped>
+
+.blog-post {
+  background-color: aqua;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
