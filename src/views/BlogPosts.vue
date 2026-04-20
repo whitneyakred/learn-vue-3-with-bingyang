63b4c0b0d32860f8447452e6f6891633777c581f
whitneyakred <whitney@akred.com>
2026-04-20 08:27:53 -0500

    transitions

---
+++ src/views/BlogPosts.vue
@@ -0,0 +1,48 @@
+<template>
+
+    <div class="blog-posts-layout">
+        <aside class="blog-post-list">
+            <BlogPostList></BlogPostList>
+        </aside>
+
+        <main class="blog-post-content-with-sidebar">
+            
+            <router-view class="blog-post-content" v-slot="{ Component, route }">
+                <transition mode="out-in"
+                    enter-active-class="animate__animated animate__rotateInDownLeft animate__faster"
+                    leave-active-class="animate__animated animate__rotateOutUpRight animate__faster">
+                    <component :is="Component" :key="route.path"></component>
+                </transition>
+            </router-view>
+            <router-view name="sidebar"></router-view>
+        </main>
+    </div>
+
+</template>
+
+<script setup>
+import BlogPostList from '@/components/BlogPostList.vue'
+</script>
+
+<style lang="scss" scoped>
+.blog-posts-layout {
+    display: flex;
+    width: 100%;
+    gap: 1rem;
+
+    .blog-post-list {
+        flex: 0 0 25%
+    }
+
+    .blog-post-content-with-sidebar {
+        display: flex;
+        flex: 0 0 75%;
+
+        .blog-post-content {
+            flex: 1; 
+        }
+    }
+}
+
+</style>
+
---
+++ src/views/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+    
+    <div class="main-layout">
+        <Navigation></Navigation>
+        <router-view v-slot="{ Component, route }">
+            <transition mode="out-in"
+                :enter-active-class="route.meta.enterAnimation || 'animate__animated animate__fadeIn'"
+                :leave-active-class="route.meta.leaveAnimation || 'animate__animated animate__fadeOut'">
+                <component :is="Component" />
+            </transition>
+        </router-view>
+    </div>
+
+</template>
+
+<script setup>
+import Navigation from '@/components/Navigation.vue';
+</script>
+
+<style lang="scss" scoped>
+
+.main-layout {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+</style>
